380ff34e032fd32e40f2a5c311162d3bec56b0b1
Muthiani Junior <jossmuth@outlook.com>
2022-09-14 03:25:00 -0700

    Amendment 4, print with printf()

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Desription: prints "with proper grammer, but the outcome is a piece of art,"
+ * Desription: prints "with proper grammar, but the outcome is a piece of art,"
  * Return: 0
  */
 
 int main(void)
 {
-	printf("%s", "with proper grammer, but the outcome is a piece of art,\n");
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
