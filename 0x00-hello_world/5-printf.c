8c7652b9477bb13d556bedeadce98386bdfe3ae5
Muthiani Junior <jossmuth@outlook.com>
2022-09-14 03:13:07 -0700

    Print with printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desription: prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	Return (0);
+}
