dedc1cdb1915dcec49b12fa9edcb8d5ad899bb48
Muthiani Junior <jossmuth@outlook.com>
2022-09-15 13:06:13 -0700

    Smallest combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdi.h>
+
+/**
+ * main - Entry point
+ * Description: Print combination of small three digit
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && J < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
