ca2b45857dc922bc1a719e2a8a0a690f2004b3c3
Muthiani Junior <jossmuth@outlook.com>
2022-09-20 12:40:12 -0700

    Amended, 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,34 @@
 
 void times_table(void)
 {
-	int n, j;
+	int row, column, product, tens, ones;
 
-	for (n = '0'; n <= '9'; n++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (column = 0; column <= 9; column++)
 		{
-			_putchar(n * j);
-			_putchar(',');
+			product = row * column;
+			tens = product / 10;
+			ones = product % 10;
+
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(ones + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(ones + '0');
+			}
 		}
 		_putchar('\n');
 	}
