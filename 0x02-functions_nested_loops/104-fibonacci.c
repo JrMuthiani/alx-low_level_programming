0e97429d87d5c4ed0603d61c1fff6ec9c27a94f3
Muthiani Junior <jossmuth@outlook.com>
2022-09-20 14:56:45 -0700

    A 2, first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * numlength - Entry point returns length of string
+ * numLength - Entry point returns length of string
  * @num: operand num
  * Return: number of digits
  */
@@ -39,7 +39,7 @@ int main(void)
 		{
 			printf("%lu", f1o);
 		}
-		initial0s = numlength(mx) - 1 - numlength(f1);
+		initial0s = numLength(mx) - 1 - numLength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%i", 0);
