601f6e22b7f5af21e473d903358ee4363f56bc75
Muthiani Junior <jossmuth@outlook.com>
2022-09-20 12:45:56 -0700

    Sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @num1: takes an integer
+ * @num2: takes an integer
+ * Description: sum of two integers
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	sum = num1 + num2;
+	return (sum);
+}
