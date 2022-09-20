65be8d644f41e241c018c831bb99035807498fb2
Muthiani Junior <jossmuth@outlook.com>
2022-09-20 13:18:57 -0700

    Amended, Multiplication tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,28 +22,28 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 				else if (z > 9)
 				{
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 				else
 				{
-					if (y != 0)
+					if (j != 0)
 					{
 						_putchar(',');
 						_putchar(32);
 						_putchar(32);
 						_putchar(32);
 					}
-					_putchar(z + '0');
+					_putchar(k + '0');
 				}
 			}
 			_putchar('\n');
