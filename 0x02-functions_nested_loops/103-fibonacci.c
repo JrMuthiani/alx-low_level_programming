db77768f1764112de46a9acc9f2596aa58202e2a
Muthiani Junior <jossmuth@outlook.com>
2022-09-20 14:10:12 -0700

    Sum of Fibonacci numbers upto 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Fibonacci numbers, sum of their even values
+ * up until 4000000
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 1;
+	int sum = 0;
+
+	for (next = 0; next < 4000000; next = a + b)
+	{
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
+	}
+	printf("%i\n", sum);
+	return (0);
+}
