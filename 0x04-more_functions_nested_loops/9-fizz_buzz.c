a3d7f5cc873296adec97eca2c67ef0cac6df7291
Muthiani Junior <jossmuth@outlook.com>
2022-11-23 20:10:33 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz in numbers
+ * Description: Print Fizz for numbers divisible by 3,
+ *			Buzz for thode divisible by 5
+ *			and FizzBuzz for those divisible by 3 and 5
+ * Return: void
+ */
+
+int main(void)
+{
+	int a = 1;
+
+	while (a <= 100)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			printf("&i", a);
+		if (a != 100)
+			putchar(' ');
+
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
