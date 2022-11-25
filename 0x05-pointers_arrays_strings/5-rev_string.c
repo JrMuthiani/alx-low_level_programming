1371b76b6d250ffd88430fd46a5a7e39f22628cf
Muthiani Junior <jossmuth@outlook.com>
2022-11-25 20:02:04 +0000

    A1,String in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,17 @@
 
 void rev_string(char *s)
 {
-	int len, a;
-	char x;
+	int len, a, half;
+	char temp;
 
-	len = 0;
-	while (*(s + len) != '0')
-		len++;
-	len--;
-	for (a = 0; a < len; a++)
+	for (len = 0; s[len] != '\0'; len++)
+	a = 0;
+	half = len / 2;
+	while (half--)
 	{
-		x = *(s + len);
-		*(s + len) = *(s + a);
-		*(s + a) = x;
-		len--;
+		temp = s[len - a - 1];
+		s[len - a - 1] = s[a];
+		s[a] = temp;
+		a++;
 	}
 }
