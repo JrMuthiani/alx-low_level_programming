6a1042bb6f947b339d22cdd300fe49a72ef94a24
Muthiani Junior <jossmuth@outlook.com>
2022-11-25 19:40:33 +0000

    String in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Print string in reverse
+ * @s: Pointer to character
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
