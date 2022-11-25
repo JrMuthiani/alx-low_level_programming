db8f87ca803e375b672d925316f3f17f914417cd
Muthiani Junior <jossmuth@outlook.com>
2022-11-25 20:08:08 +0000

    String, every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Print string, every other character
+ * @str: Pointer string to be printed
+ * Return: void
+ */
+
+void puts2(char *str);
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
