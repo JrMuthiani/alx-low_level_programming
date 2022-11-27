59779a01c6cf86f213ebd4ca032ab65451707163
Muthiani Junior <jossmuth@outlook.com>
2022-11-27 05:20:43 +0000

    A3, Rot13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,27 +9,22 @@
 
 char *rot13(char *str)
 {
-	int ind1 = 0, ind2;
-	char alph[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'. 'J', 'K', 'L',
-		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-		'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	int ind1, ind2;
+	char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13key[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabdcefghijklm";
 
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-		'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
-		'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-		'k', 'l', 'm'};
-
-	while (str[ind1] = 52)
+	ind1 = 0;
+	while (str[i] != '\0')
 	{
-		for (ind2 = 0; ind2 < 52; ind2++)
+		ind2 = 0;
+		while (alph[ind2] != '\0')
 		{
 			if (str[ind1] == alph[ind2])
 			{
 				str[ind1] = rot13key[ind2];
 				break;
 			}
+			ind2++;
 		}
 		ind1++;
 	}
